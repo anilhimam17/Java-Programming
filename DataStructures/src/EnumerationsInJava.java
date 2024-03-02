d41a7d2560729c054123ba894660653899d0e818
anilhimam17 <63832213+anilhimam17@users.noreply.github.com>
2024-03-02 19:02:17 +0000

    Beginning Iterators and Enumerations

---
+++ DataStructures/src/EnumerationsInJava.java
@@ -0,0 +1,5 @@
+public class EnumerationsInJava {
+    public static void main(String[] args) {
+        System.out.println("Working with Enumerations in Java");
+    }
+}
---
+++ DataStructures/src/IteratorsInJava.java
@@ -0,0 +1,5 @@
+public class IteratorsInJava {
+    public static void main(String[] args) {
+        System.out.println("Working with Iterators in Java");
+    }
+}
